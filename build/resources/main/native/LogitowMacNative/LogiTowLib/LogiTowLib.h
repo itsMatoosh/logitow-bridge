496fbf7c74d801bdc2ab9b6ac3fb86566465a3fb
Mateusz Rębacz <matirebacz@op.pl>
2018-01-04 23:00:28 +0100

    moved resources

---
+++ build/resources/main/native/LogitowMacNative/LogiTowLib/LogiTowLib.h
@@ -0,0 +1,15 @@
+//
+//  LogiTowLib.h
+//  LogiTowLib
+//
+//  Created by Trychen on 17/12/10.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import "controller.h"
+#import <JavaVM/jni.h>
+
+@interface LogiTowLib : NSObject
+
+@end
---
+++ build/resources/main/native/LogitowWindowsNative/Main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LogitowWindowsNative
+{
+    public class Program
+    {
+        /// <summary>
+        /// The scanner being currently used.
+        /// </summary>
+        public static Scanner scanner;
+
+        /// <summary>
+        /// Entry point of the program.
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            scanner = new Scanner();
+            scanner.StartBleDeviceWatcher();
+
+            Console.WriteLine("Press to stop device discovery...");
+            Console.ReadLine();
+            scanner.StopBleDeviceWatcher();
+        }
+    }
+}
